e548a53e5b0f8717b2f71a9099ba7f1c1198f8a0
Aleksandr Dzyubinskiy <timik_333@mail.ru>
2023-04-08 18:33:55 +0500

    feat: add functions for copy.js and create.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+let __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
-};
+    const sourceDir = path.resolve(__dirname, 'files');
+    const destDir = path.resolve(__dirname, 'files_copy');
+
+    try {
+        await fs.access(destDir);
+        throw new Error('FS operation failed');
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
+      }
+      try {
+        await fs.mkdir(destDir);
+      } catch (err) {
+        throw new Error('FS operation failed');
+      }
+      try {
+        const files = await fs.readdir(sourceDir);
+        for (const file of files) {
+          await fs.copyFile(path.join(sourceDir, file), path.join(destDir, file));
+        }
+      } catch (err) {
+        throw new Error('FS operation failed');
+      }
+    }
 
 await copy();
