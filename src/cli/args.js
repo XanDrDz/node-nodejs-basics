0aa3a0b52907c86a163fa55e37a31f2aaa7051c4
Aleksandr Dzyubinskiy <timik_333@mail.ru>
2023-04-08 20:27:16 +0500

    feat: add function for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const properties = {};
+  
+    while (args.length) {
+      const prop = args.shift().slice(2);
+      const value = args.shift(); 
+      properties[prop] = value;
+    }
+  
+    for (const [prop, value] of Object.entries(properties)) {
+      console.log(`${prop} is ${value}`);
+    }
 };
 
 parseArgs();
