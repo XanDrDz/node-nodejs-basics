549ae014ddbe37de5e8b7973ca0bdf02a5ffe395
Aleksandr Dzyubinskiy <timik_333@mail.ru>
2023-04-08 19:53:39 +0500

    feat: add function for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const variables = Object.keys(process.env).filter(key => key.startsWith(prefix));
+    if (variables.length === 0) {
+      console.log('No environment variables found');
+      return;
+    }
+
+    console.log(process.env)
+  
+    const result = variables.map(key => `RSS_${key.slice(prefix.length)}=${process.env[key]}`).join('; ');
+    console.log(result);
 };
 
 parseEnv();
